d4624f8244f3b8b72b54a277115c3ea0ef0e3a76
hagitram <ram1hagit@gmail.com>
2020-01-21 13:12:31 +0200

    1.1 Illegal hole Done

--- kalah.py
+++ kalah.py
@@ -1,6 +1,14 @@
 class Kalah:
 
+    def __init__(self, holes, seeds):
+        self.current_player = 0
+
     def status(self):
         stat = (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
         return stat
 
+    def play(self, hole_num):
+        if self.current_player == 0 and 6 < hole_num < 13:
+            return "Hole number too large"
+        if self.current_player == 1 and 0 <= hole_num < 6:
+            return "Hole number too small"
--- test_kalah.py
+++ test_kalah.py
@@ -4,6 +4,7 @@ import kalah
 #     """This is an example test. Please delete me."""
 #     game = kalah.Kalah()
 
+
 def test_initial_status():
     game1 = kalah.Kalah(6, 4)
     assert game1.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
@@ -13,31 +14,16 @@ def test_illegal_hole():
     game1 = kalah.Kalah(6, 4)
     if game1.current_player == 0:
         assert game1.play(7) == "Hole number too large"
+        assert game1.play(8) == "Hole number too large"
+        assert game1.play(9) == "Hole number too large"
+        assert game1.play(10) == "Hole number too large"
+        assert game1.play(11) == "Hole number too large"
+        assert game1.play(12) == "Hole number too large"
+    # ----------
     if game1.current_player == 1:
         assert game1.play(0) == "Hole number too small"
-    # ----------
-    if game1.current_player == 0:
-        assert game1.play(8) == "Hole number too large"
-    if game1.current_player == 1:
         assert game1.play(1) == "Hole number too small"
-    # ----------
-    if game1.current_player == 0:
-        assert game1.play(9) == "Hole number too large"
-    if game1.current_player == 1:
         assert game1.play(2) == "Hole number too small"
-    # ----------
-    if game1.current_player == 0:
-        assert game1.play(10) == "Hole number too large"
-    if game1.current_player == 1:
         assert game1.play(3) == "Hole number too small"
-    # ----------
-    if game1.current_player == 0:
-        assert game1.play(11) == "Hole number too large"
-    if game1.current_player == 1:
         assert game1.play(4) == "Hole number too small"
-    # ----------
-    if game1.current_player == 0:
-        assert game1.play(12) == "Hole number too large"
-    if game1.current_player == 1:
         assert game1.play(5) == "Hole number too small"
-    # ----------
