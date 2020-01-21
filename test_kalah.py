d624972b92cf9d4d84a2a509d527d844cfa812c6
hagitram <ram1hagit@gmail.com>
2020-01-21 13:36:17 +0200

    1.2 Simple move test

--- test_kalah.py
+++ test_kalah.py
@@ -27,3 +27,21 @@ def test_illegal_hole():
         assert game1.play(3) == "Hole number too small"
         assert game1.play(4) == "Hole number too small"
         assert game1.play(5) == "Hole number too small"
+
+
+def test_simple_move():
+    game1 = kalah.Kalah(6, 4)
+    assert game1.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+    game1.play(0)
+    assert game1.status() == (0, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+    # -----------------
+    game2 = kalah.Kalah(6, 4)
+    assert game2.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+    game2.play(1)
+    assert game2.status() == (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0)
+    # -----------------
+    game1.play(7)
+    assert game1.status() == (0, 5, 5, 5, 5, 4, 0, 0, 5, 5, 5, 5, 4, 0)
+    # -----------------
+    game2.play(8)
+    assert game2.status() == (4, 0, 5, 5, 5, 5, 0, 4, 0, 5, 5, 5, 5, 0)
