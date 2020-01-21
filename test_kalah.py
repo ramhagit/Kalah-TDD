9727571c2ed906c4de17098eb628d201f2360a98
hagitram <ram1hagit@gmail.com>
2020-01-21 11:12:22 +0200

    1.1 Initial Status

--- test_kalah.py
+++ test_kalah.py
@@ -1,6 +1,10 @@
 import kalah
 
 
-def test_create_kalah_instance():
-    """This is an example test. Please delete me."""
-    game = kalah.Kalah()
+# def test_create_kalah_instance():
+#     """This is an example test. Please delete me."""
+#     game = kalah.Kalah()
+
+def test_initial_status():
+    game1 = kalah.Kalah()
+    assert game1.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
