77b2158190c850e83abefae6318777485966de9c
hagitram <ram1hagit@gmail.com>
2020-01-23 11:56:36 +0200

    1.2 Simple move done

--- kalah.py
+++ kalah.py
@@ -2,13 +2,20 @@ class Kalah:
 
     def __init__(self, holes, seeds):
         self.current_player = 0
+        self.board = [0 if i == holes or i == 2*holes + 1 else seeds for i in range((holes + 1) * 2)]
 
     def status(self):
-        stat = (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
-        return stat
+        return tuple(self.board)
 
-    def play(self, hole_num):
-        if self.current_player == 0 and 6 < hole_num < 13:
-            return "Hole number too large"
-        if self.current_player == 1 and 0 <= hole_num < 6:
-            return "Hole number too small"
+    def play(self, hole):
+        if self.current_player == 0 and len(self.board)/2-1 < hole < len(self.board)-1:
+            raise IndexError
+            # return "Hole number too large"
+        if self.current_player == 1 and 0 <= hole < len(self.board)/2-1:
+            raise IndexError
+            # return "Hole number too small"
+        start = hole+1
+        end = start + self.board[hole]
+        self.board[hole] = 0
+        for i in range(start, end):
+            self.board[i] += 1
--- test_kalah.py
+++ test_kalah.py
@@ -1,3 +1,4 @@
+import pytest
 import kalah
 
 # def test_create_kalah_instance():
@@ -5,43 +6,45 @@ import kalah
 #     game = kalah.Kalah()
 
 
-def test_initial_status():
-    game1 = kalah.Kalah(6, 4)
-    assert game1.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+@pytest.fixture()
+def game():
+    return kalah.Kalah(6, 4)
 
 
-def test_illegal_hole():
-    game1 = kalah.Kalah(6, 4)
-    if game1.current_player == 0:
-        assert game1.play(7) == "Hole number too large"
-        assert game1.play(8) == "Hole number too large"
-        assert game1.play(9) == "Hole number too large"
-        assert game1.play(10) == "Hole number too large"
-        assert game1.play(11) == "Hole number too large"
-        assert game1.play(12) == "Hole number too large"
+def test_initial_status(game):
+    assert game.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+
+
+def test_illegal_hole(game):
+    if game.current_player == 0:
+        with pytest.raises(IndexError) as index_error:
+            game.play(7)
+            game.play(8)
+            game.play(9)
+            game.play(10)
+            game.play(11)
+            game.play(12)
+        # assert "maximum recursion" in str(index_error.value)
     # ----------
-    if game1.current_player == 1:
-        assert game1.play(0) == "Hole number too small"
-        assert game1.play(1) == "Hole number too small"
-        assert game1.play(2) == "Hole number too small"
-        assert game1.play(3) == "Hole number too small"
-        assert game1.play(4) == "Hole number too small"
-        assert game1.play(5) == "Hole number too small"
+    if game.current_player == 1:
+        with pytest.raises(IndexError):
+            game.play(0)
+            game.play(1)
+            game.play(2)
+            game.play(3)
+            game.play(4)
+            game.play(5)
 
 
-def test_simple_move():
-    game1 = kalah.Kalah(6, 4)
-    assert game1.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
-    game1.play(0)
-    assert game1.status() == (0, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+def test_simple_move(game):
+    if game.current_player == 0:
+        assert game.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+        game.play(0)
+        assert game.status() == (0, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4, 0)
     # -----------------
     game2 = kalah.Kalah(6, 4)
-    assert game2.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
-    game2.play(1)
-    assert game2.status() == (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0)
+    if game2.current_player == 0:
+        assert game2.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+        game2.play(1)
+        assert game2.status() == (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0)
     # -----------------
-    game1.play(7)
-    assert game1.status() == (0, 5, 5, 5, 5, 4, 0, 0, 5, 5, 5, 5, 4, 0)
-    # -----------------
-    game2.play(8)
-    assert game2.status() == (4, 0, 5, 5, 5, 5, 0, 4, 0, 5, 5, 5, 5, 0)
